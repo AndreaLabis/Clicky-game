170b4d04d8ce807edd92edf3ebeb7abed14921bf
Andrea Labis <sweetlila111282@aol.com>
2020-05-08 14:51:34 -0400

    Updates

---
+++ precache-manifest.fbca2066200b70efe0067a9be56194e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7f015ef8b1be11bc128da127d3367c",
+    "url": "/AndreaLabis/clicky-game.git/index.html"
+  },
+  {
+    "revision": "7873012bb7ce618b6fa5",
+    "url": "/AndreaLabis/clicky-game.git/static/css/main.5ce12cf4.chunk.css"
+  },
+  {
+    "revision": "80b137b6cf82fa8bc9bf",
+    "url": "/AndreaLabis/clicky-game.git/static/js/2.c897a142.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AndreaLabis/clicky-game.git/static/js/2.c897a142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7873012bb7ce618b6fa5",
+    "url": "/AndreaLabis/clicky-game.git/static/js/main.9329c800.chunk.js"
+  },
+  {
+    "revision": "541288d23a9dd7bbb062",
+    "url": "/AndreaLabis/clicky-game.git/static/js/runtime-main.f745ef8a.js"
+  }
+]);
